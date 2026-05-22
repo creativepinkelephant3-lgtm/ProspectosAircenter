af3b9df9d93c0c34f290f7405c63a30f53b7f527
creativepinkelephant3-lgtm <creativepinkelephant3@gmail.com>
2026-05-21 21:44:41 -0500

    Update config.js
    url supabase

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Copia aquí la URL y la ANON/PUBLISHABLE KEY de tu proyecto Supabase.
 // Esta llave puede estar en el navegador SIEMPRE que tengas RLS activo en las tablas.
 window.APP_CONFIG = {
-  SUPABASE_URL: 'https://wwokowymqgozwuagekag.supabase.co/rest/v1/',
+  SUPABASE_URL: 'https://wwokowymqgozwuagekag.supabase.co',
   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3b2tvd3ltcWdvend1YWdla2FnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0MDI1NTAsImV4cCI6MjA5NDk3ODU1MH0.gfM586biF_hasFU4OH8VaDGXT3QhlygENt9GEbfHqMA'
 };
